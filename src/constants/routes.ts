76ce84325d184a31d5def4365bcf7ca1cb3339fd
Doesn'tmeananything <andrey.krasnov.pm@gmail.com>
2021-08-14 17:05:27 +0300

    feat: add program and vod routes

--- src/constants/routes.ts
+++ src/constants/routes.ts
@@ -1,7 +1,12 @@
-// Various routes of the app
-
+/**
+ * Various routes of the app
+ */
 const ROUTES = {
     home: '/',
+    programs: '/tv',
+    programDetails: '/tv/:id',
+    vods: '/movie',
+    vodDetails: '/movie/:id',
 };
 
 export default ROUTES;
