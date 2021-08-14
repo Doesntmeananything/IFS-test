1b2c9e58a2968e5238ea8eca088ff607620dd3aa
Doesn'tmeananything <andrey.krasnov.pm@gmail.com>
2021-08-14 16:54:29 +0300

    refactor: remove unused files

--- src/hooks/index.ts
+++
@@ -1,6 +0,0 @@
-import { TypedUseSelectorHook, useDispatch, useSelector } from 'react-redux';
-import { AppDispatch, RootState } from '../redux';
-
-// Use throughout your app instead of plain `useDispatch` and `useSelector`
-export const useAppDispatch = () => useDispatch<AppDispatch>() // Export a hook that can be reused to resolve types
-export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
