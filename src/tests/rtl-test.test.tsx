f846e749ab0bc9c22e5d70f36ea65b77ce773bb2
Doesn'tmeananything <andrey.krasnov.pm@gmail.com>
2021-08-14 17:10:08 +0300

    test: add basic home page tests

--- src/tests/rtl-test.test.tsx
+++ src/tests/rtl-test.test.tsx
@@ -1,9 +1,56 @@
+import { ChakraProvider } from '@chakra-ui/react';
 import '@testing-library/jest-dom/extend-expect';
-import { render, screen } from '@testing-library/react';
-import React from 'react';
-import Home from '../pages/home';
+import { fireEvent, render, screen, waitFor } from '@testing-library/react';
+import { ConnectedRouter as Router } from 'connected-react-router';
+import { createMemoryHistory } from 'history';
+import { I18nextProvider } from 'react-i18next';
+import { Provider } from 'react-redux';
+import { Route, Switch } from 'react-router-dom';
 
-test("Should agreed a div contain 'It works.' inside", () => {
-    render(<Home />);
-    expect(screen.getByText('It works.')).toBeInTheDocument();
+import i18next from 'src/configs/i18n';
+import ROUTES from 'src/constants/routes';
+import { Home } from 'src/pages/Home';
+import { Program } from 'src/pages/Program';
+import { Vod } from 'src/pages/Vod';
+import store from 'src/redux';
+import { theme } from 'src/theme';
+
+const App = () => {
+    const history = createMemoryHistory();
+
+    return (
+        <Provider store={store}>
+            <Router history={history}>
+                <I18nextProvider i18n={i18next}>
+                    <ChakraProvider theme={theme}>
+                        <Switch>
+                            <Route exact path={ROUTES.home} component={Home} />
+                            <Route exact path={ROUTES.programDetails} component={Program} />
+                            <Route exact path={ROUTES.vodDetails} component={Vod} />
+                        </Switch>
+                    </ChakraProvider>
+                </I18nextProvider>
+            </Router>
+        </Provider>
+    );
+};
+describe('Home page', () => {
+    test('contains the Home link text in the header', () => {
+        render(<App />);
+        expect(screen.getByText('Home')).toBeInTheDocument();
+    });
+
+    test('contains "Scheduled On TV" message in the programs section', () => {
+        render(<App />);
+        expect(screen.getByText('Scheduled On TV')).toBeInTheDocument();
+    });
+
+    test('allows for navigating to program details page', async () => {
+        render(<App />);
+
+        const [programLink] = await screen.findAllByTestId('program');
+        fireEvent.click(programLink);
+
+        await waitFor(() => expect(screen.getByTestId('program-details')).toBeInTheDocument());
+    });
 });
